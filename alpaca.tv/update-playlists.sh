76a1b9d2ef38e6a3aec4f42baa2ae49f03cf74d9
vi <digitaimadness@pm.me>
2022-08-16 12:19:04 +0300

    autoupdate playists

--- alpaca.tv/update-playlists.sh
+++ alpaca.tv/update-playlists.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/sh
-curl --socks5-hostname 127.0.0.1:9050 --compressed -o ./ace.ru.m3u http://f27uk3gyl2gfu4z36eifv4ob73w6xgrcms4w4vdxzcsxsobgc766ityd.onion/trash/ttv-list/acetv.all.tag.iproxy.m3u
-curl --socks5-hostname 127.0.0.1:9050 --compressed -o ./acestream.m3u http://f27uk3gyl2gfu4z36eifv4ob73w6xgrcms4w4vdxzcsxsobgc766ityd.onion/trash/ttv-list/ace.all.tag.iproxy.m3u
+curl --socks5-hostname 127.0.0.1:9050 --compressed -o ./ace.ru.m3u http://f27uk3gyl2gfu4z36eifv4ob73w6xgrcms4w4vdxzcsxsobgc766ityd.onion/trash/ttv-list/acetv.all.iproxy.m3u
+curl --socks5-hostname 127.0.0.1:9050 --compressed -o ./acestream.m3u http://f27uk3gyl2gfu4z36eifv4ob73w6xgrcms4w4vdxzcsxsobgc766ityd.onion/trash/ttv-list/ace.all.iproxy.m3u
 curl --socks5-hostname 127.0.0.1:9050 --compressed -o ./ace.world.m3u http://f27uk3gyl2gfu4z36eifv4ob73w6xgrcms4w4vdxzcsxsobgc766ityd.onion/trash/ttv-list/as.all.iproxy.m3u
 curl --socks5-hostname 127.0.0.1:9050 --compressed -o ./direct.m3u http://f27uk3gyl2gfu4z36eifv4ob73w6xgrcms4w4vdxzcsxsobgc766ityd.onion/trash/ttv-list/as.direct.m3u
 cat acestream.m3u >> ace.ru.m3u
