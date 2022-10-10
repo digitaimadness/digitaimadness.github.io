d170f3d009127d0f6f949fe5e9ce264bad663bc8
digitaimadness <digitaimadness@pm.me>
2022-10-10 14:05:57 +0300

    autoupdate playists

--- alpaca.tv/update-playlists.sh
+++ alpaca.tv/update-playlists.sh
@@ -2,8 +2,8 @@
 curl --socks5-hostname 127.0.0.1:9050 --compressed -o ./ace.ru.m3u http://f27uk3gyl2gfu4z36eifv4ob73w6xgrcms4w4vdxzcsxsobgc766ityd.onion/trash/ttv-list/ace.all.tag.iproxy.m3u
 curl --socks5-hostname 127.0.0.1:9050 --compressed -o ./ace.world.m3u http://f27uk3gyl2gfu4z36eifv4ob73w6xgrcms4w4vdxzcsxsobgc766ityd.onion/trash/ttv-list/as.all.iproxy.m3u
 curl --socks5-hostname 127.0.0.1:9050 --compressed -o ./direct.m3u http://f27uk3gyl2gfu4z36eifv4ob73w6xgrcms4w4vdxzcsxsobgc766ityd.onion/trash/ttv-list/as.direct.m3u
-sed -i 's/127.0.0.1/192.168.1.2/g' ace.ru.m3u
-sed -i 's/127.0.0.1/192.168.1.2/g' ace.world.m3u
+sed -i 's/127.0.0.1/192.168.1.7/g' ace.ru.m3u
+sed -i 's/127.0.0.1/192.168.1.7/g' ace.world.m3u
 
 git commit -a -m "autoupdate playists"
 git push
